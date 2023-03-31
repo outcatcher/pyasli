bcc303e18a65c281d1a3dc7f1fb31499e94a0cad
Anton Kachurin <katchuring@gmail.com>
2023-04-01 01:13:01 +0300

    Fix tests

--- pyasli/browsers/__init__.py
+++ pyasli/browsers/__init__.py
@@ -1,2 +1,5 @@
 """Browser wrapper implementation"""
 
+# Those imports are here to simplify access
+from .browser_session import BrowserSession  # noqa: F401
+from .shared_browser import browser, set_browser  # noqa: F401
